a829ab56b88a4de3096d2961efdec1cb456398fe
Даниил Шерстенников <70951451+SherstennikovDaniil@users.noreply.github.com>
2023-08-08 02:20:26 +0300

    added unique attr to username field

--- website/users/models.py
+++ website/users/models.py
@@ -27,7 +27,9 @@ class UserManage(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=50, blank=True)
+    username = models.CharField(
+        max_length=50, blank=True, null=True, unique=True
+    )
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     USERNAME_FIELD = "email"
