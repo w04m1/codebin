607e867d1874ee0ae3f7457722b3a7c0c70111c7
Даниил Шерстенников <70951451+SherstennikovDaniil@users.noreply.github.com>
2023-08-08 02:06:53 +0300

    added new serializer to views and to urls

--- website/users/views.py
+++ website/users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from .serializers import CustomTokenObtainPairSerializer
 
-# Create your views here.
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
--- website/website/urls.py
+++ website/website/urls.py
@@ -16,14 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from users.views import CustomTokenObtainPairView
 
 urlpatterns = [
     path(
-        "api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+        "api/token/",
+        CustomTokenObtainPairView.as_view(),
+        name="token_obtain_pair",
     ),
     path(
         "api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
