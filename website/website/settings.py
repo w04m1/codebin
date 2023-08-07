990961eb9e72d1947b7bf3a018e5b2c558db8b4d
Даниил Шерстенников <70951451+SherstennikovDaniil@users.noreply.github.com>
2023-08-07 23:55:09 +0300

    added custom user model as auth model

--- website/website/settings.py
+++ website/website/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "users.User"
