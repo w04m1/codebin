edebbb4b34ed32f59f801afe1232e19c22142389
Даниил Шерстенников <70951451+SherstennikovDaniil@users.noreply.github.com>
2023-08-08 00:59:52 +0300

    added url for jwt token management

--- website/website/urls.py
+++ website/website/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path(
+        "api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
+    path(
+        "api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
+    ),
     path("admin/", admin.site.urls),
 ]
