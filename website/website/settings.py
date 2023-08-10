f4c20be98a4080d1554ff78b655aa77b203cbe22
Даниил Шерстенников <70951451+SherstennikovDaniil@users.noreply.github.com>
2023-08-10 05:14:08 +0300

    upd settings for cors handling

--- website/website/settings.py
+++ website/website/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "users.apps.UsersConfig",
     "snippets.apps.SnippetsConfig",
 ]
@@ -78,9 +79,12 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
 }
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
